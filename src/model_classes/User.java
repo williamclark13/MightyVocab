2f44cbc0723ef7bff13502c6737550a03b179678
William Clark <wolfclark13@gmail.com>
2023-02-14 13:22:18 -0500

    Changed Access Level for User Setters

--- src/model_classes/User.java
+++ src/model_classes/User.java
@@ -2,15 +2,17 @@ package model_classes;
 
 /**
  * User Class
+ * 
  * @author William Clark
  * @version 2/6/2023
  */
 public class User {
 	private String username;
 	private String password;
-	
+
 	/**
 	 * Instantiates an instance of a User
+	 * 
 	 * @param username
 	 * @param password
 	 */
@@ -18,21 +20,23 @@ public class User {
 		this.setUsername(username);
 		this.setPassword(password);
 	}
-	
+
 	/**
 	 * Gets username
+	 * 
 	 * @return username
 	 */
 	public String GetUsername() {
 		return this.username;
 	}
-	
+
 	/**
 	 * Functionality to set the username
+	 * 
 	 * @param username
 	 * @return true if username is being set
 	 */
-	public boolean setUsername(String username) {
+	private boolean setUsername(String username) {
 		if (username == null) {
 			throw new IllegalArgumentException("Username cannot be null.");
 		}
@@ -42,21 +46,23 @@ public class User {
 		this.username = username;
 		return true;
 	}
-	
+
 	/**
 	 * Gets password
+	 * 
 	 * @return password
 	 */
 	public String GetPassword() {
 		return this.password;
 	}
-	
+
 	/**
 	 * Functionality to set the password
+	 * 
 	 * @param password
 	 * @return true if password is being set
 	 */
-	public boolean setPassword(String password) {
+	private boolean setPassword(String password) {
 		if (password == null) {
 			throw new IllegalArgumentException("Password cannot be null.");
 		}
@@ -66,14 +72,15 @@ public class User {
 		this.password = password;
 		return true;
 	}
-	
+
 	/**
 	 * Verify user has valid credentials
+	 * 
 	 * @param username
 	 * @param password
 	 * @return true if valid credentials, false otherwise
 	 */
 	public boolean verifyCredentials(String username, String password) {
-		return  this.username.equals(username) && this.password.equals(password);
+		return this.username.equals(username) && this.password.equals(password);
 	}
 }
