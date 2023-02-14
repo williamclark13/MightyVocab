3284e6a64cd570d568a742247886ff68e5b125ae
Michael  Lee <mlee16@my.westga.edu>
2023-02-13 22:38:38 -0500

    Created Account Manager class and resources class.

---
+++ src/resources/ResourceMessages.java
@@ -0,0 +1,7 @@
+package resources;
+
+public class ResourceMessages {
+
+	public static final String KEY_NULL = "Key cannot be null";
+	public static final String VALUE_NULL = "Value cannot be null";
+}
