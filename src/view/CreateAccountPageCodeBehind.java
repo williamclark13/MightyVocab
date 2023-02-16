021b24245a4f071dcc4a8ae3a83c211baa0d339c
racheleckle <116831464+racheleckle@users.noreply.github.com>
2023-02-15 20:21:09 -0500

    Create 'Create Account' Page UI

---
+++ src/view/CreateAccountPageCodeBehind.java
@@ -0,0 +1,36 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class CreateAccountPageCodeBehind {
+
+	@FXML
+	private Button createAccountButton;
+
+	@FXML
+	private Label invalidCredentialsLabel;
+
+	@FXML
+	private Hyperlink loginpageHyperlink;
+
+	@FXML
+	private PasswordField passwordPasswordField;
+
+	@FXML
+	private TextField usernameTextField;
+
+	public CreateAccountPageCodeBehind() {
+
+	}
+
+	@FXML
+	private void initialize() {
+
+	}
+
+}
