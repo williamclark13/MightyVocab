4b8f52f9badbeaff644836c1c0e89d2359172d25
Michael  Lee <mlee16@my.westga.edu>
2023-02-23 00:23:29 -0500

    Added functionality for switching between scenes

--- src/view/CreateAccountPageCodeBehind.java
+++ src/view/CreateAccountPageCodeBehind.java
@@ -1,13 +1,24 @@
 package view;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import viewmodel.LoginViewModel;
 
 public class CreateAccountPageCodeBehind {
+
+	private LoginViewModel viewModel;
 
 	@FXML
 	private Button createAccountButton;
@@ -25,12 +36,27 @@ public class CreateAccountPageCodeBehind {
 	private TextField usernameTextField;
 
 	public CreateAccountPageCodeBehind() {
-
+		this.viewModel = new LoginViewModel();
 	}
 
 	@FXML
 	private void initialize() {
+		
+		this.bindToLoginViewModel();
+	}
 
+	private void bindToLoginViewModel() {
+		this.usernameTextField.textProperty().bindBidirectional(this.viewModel.usernameProperty());
+		this.passwordPasswordField.textProperty().bindBidirectional(this.viewModel.passwordProperty());
+	}
+
+	@FXML
+	void goToLoginPage(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("../view/LoginPage.fxml"));
+		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
 
 }
--- src/viewmodel/LoginViewModel.java
+++ src/viewmodel/LoginViewModel.java
@@ -1,12 +1,7 @@
 package viewmodel;
 
-import java.io.IOException;
-
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 
 public class LoginViewModel {
 	private StringProperty usernameProperty;
@@ -28,27 +23,6 @@ public class LoginViewModel {
 	// Check for username and password being correct and in file.
 	// If on Create Account page then on button press create method to handle adding username and password to file
 	// Maybe move these methods below to code behind since it deals with fxml, instead just focus on saving to file and checking file for name
-	
-	private void goToCreateAccountPage() {
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("newFxml.fxml"));
-		try {
-			Parent root = (Parent) fxmlLoader.load();
-			Scene scene = new Scene(root, 600, 65);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void goToLoginPage() {
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("newFxml.fxml"));
-		try {
-			Parent root = (Parent) fxmlLoader.load();
-			Scene scene = new Scene(root, 600, 65);
-			//jfxPanel.setScene(scene);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 	
 	
 }
