b22b8fb1fbe7b59f16c5d33fa8cf6a17a9786966
racheleckle <116831464+racheleckle@users.noreply.github.com>
2023-02-22 22:13:54 -0500

    Implement dialog functionality on the Notecards page.

--- src/view/NotecardsPageCodeBehind.java
+++ src/view/NotecardsPageCodeBehind.java
@@ -5,14 +5,31 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import model_classes.Notecard;
 
@@ -39,6 +56,9 @@ public class NotecardsPageCodeBehind {
 
 	@FXML
 	private MenuItem logoutMenuItem;
+
+	@FXML
+	private MenuButton menuMenuButton;
 
 	@FXML
 	private MenuItem notecardsMenuItem;
@@ -87,21 +107,114 @@ public class NotecardsPageCodeBehind {
 	@FXML
 	void createNotecard(ActionEvent event) {
 
+		Label createNotecardLabel = new Label("Create Notecard");
+		Label termLabel = new Label("Term:");
+		TextField termTextField = new TextField();
+		Label definitionLabel = new Label("Definition:");
+		TextArea definitionTextArea = new TextArea();
+		Button confirmButton = new Button("Confirm");
+		Button cancelButton = new Button("Cancel");
+		Font font = Font.font("Cambria", 18);
+		Font fontBig = Font.font("Cambria", FontWeight.BOLD, 24);
+
+		createNotecardLabel.setFont(fontBig);
+		createNotecardLabel.setTextFill(Color.DARKSLATEGREY);
+		termLabel.setFont(font);
+		termLabel.setTextFill(Color.DARKSLATEGREY);
+		definitionLabel.setFont(font);
+		definitionLabel.setTextFill(Color.DARKSLATEGREY);
+		confirmButton.setFont(font);
+		confirmButton.setTextFill(Color.DARKSLATEGREY);
+		cancelButton.setFont(font);
+		cancelButton.setTextFill(Color.DARKSLATEGREY);
+
+		VBox pane = new VBox(15);
+		pane.setPadding(new Insets(75, 100, 75, 100));
+		pane.setAlignment(Pos.CENTER);
+		pane.getChildren().addAll(createNotecardLabel, termLabel, termTextField, definitionLabel, definitionTextArea,
+				confirmButton, cancelButton);
+
+		Scene scene = new Scene(new Group(pane), Color.ALICEBLUE);
+		stage = new Stage();
+		stage.setTitle("Create Notecard");
+		stage.setScene(scene);
+		stage.show();
+
+		confirmButton.setOnAction(e -> {
+			stage.close();
+		});
+		cancelButton.setOnAction(e -> {
+			stage.close();
+		});
 	}
 
 	@FXML
 	void deleteNotecard(ActionEvent event) {
 
+		Dialog<String> confirmationDialog = new Dialog<String>();
+		confirmationDialog.setTitle("Delete Notecard");
+		ButtonType deleteButton = new ButtonType("Delete", ButtonData.NO);
+		ButtonType cancelButton = new ButtonType("Cancel", ButtonData.YES);
+		confirmationDialog.setContentText("Are you sure you want to delete this notecard?" + System.lineSeparator()
+				+ System.lineSeparator() + "Term: " + "{notecard term}" + System.lineSeparator()
+				+ System.lineSeparator() + "Definition:" + System.lineSeparator() + "{notecard definition}");
+		confirmationDialog.getDialogPane().getButtonTypes().addAll(deleteButton, cancelButton);
+
+		confirmationDialog.showAndWait();
 	}
 
 	@FXML
 	void editNotecard(ActionEvent event) {
 
+		Label createNotecardLabel = new Label("Edit Notecard");
+		Label termLabel = new Label("Term:");
+		TextField termTextField = new TextField("{notecard term}");
+		Label definitionLabel = new Label("Definition:");
+		TextArea definitionTextArea = new TextArea("{notecard definition}");
+		Button confirmButton = new Button("Confirm");
+		Button cancelButton = new Button("Cancel");
+		Font font = Font.font("Cambria", 18);
+		Font fontBig = Font.font("Cambria", FontWeight.BOLD, 24);
+
+		createNotecardLabel.setFont(fontBig);
+		createNotecardLabel.setTextFill(Color.DARKSLATEGREY);
+		termLabel.setFont(font);
+		termLabel.setTextFill(Color.DARKSLATEGREY);
+		definitionLabel.setFont(font);
+		definitionLabel.setTextFill(Color.DARKSLATEGREY);
+		confirmButton.setFont(font);
+		confirmButton.setTextFill(Color.DARKSLATEGREY);
+		cancelButton.setFont(font);
+		cancelButton.setTextFill(Color.DARKSLATEGREY);
+
+		VBox pane = new VBox(15);
+		pane.setPadding(new Insets(75, 100, 75, 100));
+		pane.setAlignment(Pos.CENTER);
+		pane.getChildren().addAll(createNotecardLabel, termLabel, termTextField, definitionLabel, definitionTextArea,
+				confirmButton, cancelButton);
+
+		Scene scene = new Scene(new Group(pane), Color.ALICEBLUE);
+		stage = new Stage();
+		stage.setTitle("Edit Notecard");
+		stage.setScene(scene);
+		stage.show();
+
+		confirmButton.setOnAction(e -> {
+			stage.close();
+		});
+		cancelButton.setOnAction(e -> {
+			stage.close();
+		});
 	}
 
 	@FXML
-	void goToNotecardsPage(ActionEvent event) {
+	void goToNotecardsPage(ActionEvent event) throws IOException {
 
+		root = FXMLLoader.load(getClass().getResource("NotecardsPage.fxml"));
+		stage = (Stage) this.menuMenuButton.getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
 
 	@FXML
@@ -130,13 +243,59 @@ public class NotecardsPageCodeBehind {
 	}
 
 	@FXML
-	void userLogout(ActionEvent event) {
+	void userLogout(ActionEvent event) throws IOException {
 
+		root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+		stage = (Stage) this.menuMenuButton.getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+
+		Dialog<String> confirmationDialog = new Dialog<String>();
+		confirmationDialog.setTitle("Logged Out");
+		ButtonType okButton = new ButtonType("OK", ButtonData.OK_DONE);
+		confirmationDialog.setContentText("Successfully logged out. See you later!");
+		confirmationDialog.getDialogPane().getButtonTypes().addAll(okButton);
+
+		confirmationDialog.showAndWait();
 	}
 
 	@FXML
 	void viewNotecard(ActionEvent event) {
 
+		Label createNotecardLabel = new Label("Notecard");
+		Label termLabel = new Label("Term: " + "{notecard term}");
+		Label definitionLabel = new Label("Definition:");
+		Label notecardDefinitionLabel = new Label("{notecard definition}");
+		Button okButton = new Button("OK");
+		Font font = Font.font("Cambria", 18);
+		Font fontBig = Font.font("Cambria", FontWeight.BOLD, 24);
+
+		createNotecardLabel.setFont(fontBig);
+		createNotecardLabel.setTextFill(Color.DARKSLATEGREY);
+		termLabel.setFont(font);
+		termLabel.setTextFill(Color.DARKSLATEGREY);
+		definitionLabel.setFont(font);
+		definitionLabel.setTextFill(Color.DARKSLATEGREY);
+		notecardDefinitionLabel.setFont(font);
+		notecardDefinitionLabel.setTextFill(Color.DARKSLATEGREY);
+		okButton.setFont(font);
+		okButton.setTextFill(Color.DARKSLATEGREY);
+
+		VBox pane = new VBox(15);
+		pane.setPadding(new Insets(75, 100, 75, 100));
+		pane.setAlignment(Pos.CENTER);
+		pane.getChildren().addAll(createNotecardLabel, termLabel, definitionLabel, notecardDefinitionLabel, okButton);
+
+		Scene scene = new Scene(new Group(pane), Color.ALICEBLUE);
+		stage = new Stage();
+		stage.setTitle("View Notecard");
+		stage.setScene(scene);
+		stage.show();
+
+		okButton.setOnAction(e -> {
+			stage.close();
+		});
 	}
 
 }
